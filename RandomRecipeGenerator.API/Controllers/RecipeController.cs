f042584faddf9fe6db5a196544912eda3a9c8859
cw222kq <lotten_w@hotmail.com>
2025-03-22 21:20:21 +0100

    Initial commit

---
+++ RandomRecipeGenerator.API/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RandomRecipeGenerator.API.Services;
+
+namespace RandomRecipeGenerator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController(HttpRequestService httpRequestService) : ControllerBase
+    {
+        private readonly HttpRequestService _httpRequestService = httpRequestService;
+
+        // GET: api/recipe
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _httpRequestService.GetAsync("https://api.spoonacular.com/recipes/random?number=1&includeNutrition=false");
+
+            return Ok(result);
+        }
+    }
+}
---
+++ RandomRecipeGenerator.API/Services/HttpRequestService.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+
+namespace RandomRecipeGenerator.API.Services
+{
+    public class HttpRequestService(HttpClient httpClient)
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public async Task<string> GetAsync(string url)
+        { 
+            try
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, url);
+                var response = await _httpClient.SendAsync(request);
+
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"An error occurred: {e.Message}");
+                throw;
+            }
+        }
+
+    }
+}
