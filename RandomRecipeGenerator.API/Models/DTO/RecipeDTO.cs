07ff034d5daa63979582329c69fbd43659b0fc6c
cw222kq <lotten_w@hotmail.com>
2025-03-24 17:38:20 +0100

    Add models

---
+++ RandomRecipeGenerator.API/Models/DTO/RecipeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomRecipeGenerator.API.Models.DTO
+{
+    public class RecipeDTO
+    {
+        public required string Name { get; set; }
+        public required string Ingredients { get; set; }
+        public required string Instructions { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ RandomRecipeGenerator.API/Models/Domain/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomRecipeGenerator.API.Models.Domain
+{
+    public class Recipe
+    {
+        public Guid Id { get; set; }
+        public  required int SpoonacularId { get; set; }
+        public required string Name { get; set; }
+        public required string Ingredients { get; set; }
+        public required string Instructions { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
