25736a844b4aee969d13133069e2113a92c3c15d
cw222kq <lotten_w@hotmail.com>
2025-04-15 14:06:19 +0200

    Add frontend for mobile

---
+++ random-recipe-mobile/App.tsx
@@ -0,0 +1,14 @@
+import './global.css' // Import Tailwind directives
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="bg-white justify-center items-center h-full">
+      <Text className="text-red-500 text-2xl font-bold text-center">
+        Open up App.tsx to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
