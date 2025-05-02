d61b44d7473420c1e07878e0051a91be9da56a29
cw222kq <lotten_w@hotmail.com>
2025-05-02 18:02:42 +0200

    Refactor: Update logo in Navbar

--- frontend/random-recipe-router-mobile/components/Navbar.tsx
+++ frontend/random-recipe-router-mobile/components/Navbar.tsx
@@ -1,16 +1,10 @@
 import { View, Text } from 'react-native'
 import { Link } from 'expo-router'
-import FoodLogo from './icons/FoodLogo'
+import RecipeFinderLogo from './icons/RecipeFinderLogo'
 export const Navbar = () => {
   return (
     <View className="flex-row items-center justify-between border-b border-gray-100 py-5">
-      <View className="flex-row items-center gap-1">
-        <Text className="text-lg font-bold text-black dark:text-white">
-          Recipe
-        </Text>
-        <FoodLogo />
-        <Text className="text-lg font-bold text-blue-500">Finder</Text>
-      </View>
+      <RecipeFinderLogo />
 
       <View className="flex-row items-center gap-6">
         <Link href="/">
---
+++ frontend/random-recipe-router-mobile/components/icons/RecipeFinderLogo.tsx
@@ -0,0 +1,28 @@
+import Svg, { Path } from 'react-native-svg'
+import { useColorScheme, View, Text, ViewProps } from 'react-native'
+
+const DEFAULT_COLOR_DARK = '#000000'
+const DEFAULT_COLOR_LIGHT = '#ffffff'
+const DEFAULT_HEIGHT = 24
+const DEFAULT_WIDTH = 24
+
+export default function RecipeFinderLogo({ ...rest }: ViewProps) {
+  const colorScheme = useColorScheme()
+
+  return (
+    <View className="flex-row items-center gap-1" {...rest}>
+      <Text className="text-lg font-bold text-black dark:text-white">
+        Recipe
+      </Text>
+      <Svg
+        height={DEFAULT_HEIGHT}
+        viewBox="0 -960 960 960"
+        width={DEFAULT_WIDTH}
+        fill={colorScheme === 'dark' ? DEFAULT_COLOR_LIGHT : DEFAULT_COLOR_DARK}
+      >
+        <Path d="m175-120-56-56 410-410q-18-42-5-95t57-95q53-53 118-62t106 32q41 41 32 106t-62 118q-42 44-95 57t-95-5l-50 50 304 304-56 56-304-302-304 302Zm118-342L173-582q-54-54-54-129t54-129l248 250-128 128Z" />
+      </Svg>
+      <Text className="text-lg font-bold text-blue-500">Finder</Text>
+    </View>
+  )
+}
