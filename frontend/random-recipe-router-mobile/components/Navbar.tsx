61bc4916f2ccbdfd48bb6f781de6f745bb5eb547
cw222kq <lotten_w@hotmail.com>
2025-05-02 18:05:57 +0200

    Update the name of the application

--- frontend/random-recipe-router-mobile/components/Navbar.tsx
+++ frontend/random-recipe-router-mobile/components/Navbar.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import { Link } from 'expo-router'
-import RecipeFinderLogo from './icons/RecipeFinderLogo'
+import RecipeGeneratorLogo from './icons/RecipeGeneratorLogo'
 export const Navbar = () => {
   return (
     <View className="flex-row items-center justify-between border-b border-gray-100 py-5">
-      <RecipeFinderLogo />
+      <RecipeGeneratorLogo />
 
       <View className="flex-row items-center gap-6">
         <Link href="/">
