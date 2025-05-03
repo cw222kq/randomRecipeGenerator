47243611bf4a8a22856d4879e2d8887fc386499a
cw222kq <lotten_w@hotmail.com>
2025-05-03 15:50:26 +0200

    Add layout and styling to RecipeCard

--- frontend/random-recipe-web/src/components/RecipeCard.tsx
+++ frontend/random-recipe-web/src/components/RecipeCard.tsx
@@ -7,6 +7,7 @@ import {
   CardFooter,
 } from '@/components/ui/card'
 import { Recipe } from '@/schemas/recipeSchema'
+import Image from 'next/image'
 
 interface RecipeCardProps {
   recipe: Recipe
@@ -14,17 +15,41 @@ interface RecipeCardProps {
 
 export default function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <Card>
+    <Card className="mx-auto w-full">
       <CardHeader>
         <CardTitle>{recipe.title}</CardTitle>
         <CardDescription>Description</CardDescription>
       </CardHeader>
       <CardContent>
-        {recipe.ingredients}
-        {recipe.instructions}
-        {recipe.imageUrl}
+        <div>
+          <Image
+            src={recipe.imageUrl}
+            alt={recipe.title}
+            className="h-auto w-full rounded-t-lg object-cover"
+            priority
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="mt-4 rounded-lg border px-4 py-4">
+          <h3 className="text-lg font-semibold">Ingredients:</h3>
+          <ul>
+            {recipe.ingredients.map((ingredient, i) => (
+              <li key={i}>{ingredient}</li>
+            ))}
+          </ul>
+          {recipe.ingredients}
+        </div>
+        <div className="mt-4 rounded-lg border px-4 py-4">
+          <h3 className="text-lg font-semibold">Instructions:</h3>
+          {recipe.instructions}
+        </div>
       </CardContent>
-      <CardFooter>Footer</CardFooter>
+      <CardFooter>
+        <div className="flex w-full items-center justify-center border-t px-4 py-4 text-center">
+          Footer
+        </div>
+      </CardFooter>
     </Card>
   )
 }
