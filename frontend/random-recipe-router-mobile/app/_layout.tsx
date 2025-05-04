23b31088a03a07210e360bf1ae08b1aaa7c02f07
cw222kq <lotten_w@hotmail.com>
2025-05-04 20:06:03 +0200

    Add service to fetch random recipe

--- frontend/random-recipe-router-mobile/app/_layout.tsx
+++ frontend/random-recipe-router-mobile/app/_layout.tsx
@@ -5,11 +5,8 @@ import { StatusBar } from 'expo-status-bar'
 import { Navbar } from '@/components/Navbar'
 export default function RootLayout() {
   return (
-    // SafeAreaView helps avoid notches/status bars
     <SafeAreaView style={{ flex: 1 }}>
-      {/* Configure the status bar */}
       <StatusBar style="auto" />
-      {/* Renders the currently active child screen */}
       <Navbar />
       <Slot />
     </SafeAreaView>
--- frontend/random-recipe-router-mobile/app/index.tsx
+++ frontend/random-recipe-router-mobile/app/index.tsx
@@ -1,6 +1,17 @@
 import { View, Text } from 'react-native'
+import getRandomRecipe from '../services/recipeService'
+import { useEffect } from 'react'
 
 export default function HomeScreen() {
+  useEffect(() => {
+    const testGetRandomRecipe = async () => {
+      const result = await getRandomRecipe()
+      console.log('result')
+      console.log(result)
+    }
+    testGetRandomRecipe()
+  }, [])
+
   return (
     <View className="py-6">
       <Text className="text-black dark:text-white text-3xl font-bold">
