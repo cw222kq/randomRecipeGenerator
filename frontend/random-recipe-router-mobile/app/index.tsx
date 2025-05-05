177ba4b4c998f93b4e56e86249a396011a24fc91
cw222kq <lotten_w@hotmail.com>
2025-05-05 16:59:34 +0200

    Add Card component and setup it up manually

--- frontend/random-recipe-router-mobile/app/index.tsx
+++ frontend/random-recipe-router-mobile/app/index.tsx
@@ -1,6 +1,14 @@
 import { View, Text } from 'react-native'
 import getRandomRecipe from '../services/recipeService'
 import { useEffect } from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '../components/ui/card'
 
 export default function HomeScreen() {
   useEffect(() => {
@@ -17,6 +25,18 @@ export default function HomeScreen() {
       <Text className="text-black dark:text-white text-3xl font-bold">
         Fetched Random Recipe
       </Text>
+      <Card className="m-4">
+        <CardHeader>
+          <CardTitle>Manual Setup Card</CardTitle>
+          <CardDescription>This card was added manually.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Text>This is the main content area of the card.</Text>
+        </CardContent>
+        <CardFooter>
+          <Text>Footer content goes here.</Text>
+        </CardFooter>
+      </Card>
     </View>
   )
 }
