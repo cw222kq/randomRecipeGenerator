789cfa320b4ae5749a836221d042f8cdd9bfed9f
cw222kq <lotten_w@hotmail.com>
2025-05-05 21:36:05 +0200

    Add RecipeCard Component with some styling

--- frontend/random-recipe-router-mobile/app/index.tsx
+++ frontend/random-recipe-router-mobile/app/index.tsx
@@ -1,42 +1,31 @@
 import { View, Text } from 'react-native'
 import getRandomRecipe from '../services/recipeService'
-import { useEffect } from 'react'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '../components/ui/card'
+import { useEffect, useState } from 'react'
+import RecipeCard from '../components/RecipeCard'
+import { Recipe } from '@/schemas/recipeSchema'
 
 export default function HomeScreen() {
+  const [recipe, setRecipe] = useState<Recipe | null>(null)
   useEffect(() => {
     const testGetRandomRecipe = async () => {
       const result = await getRandomRecipe()
       console.log('result')
       console.log(result)
+      setRecipe(result)
     }
     testGetRandomRecipe()
   }, [])
 
+  if (!recipe) {
+    return <Text>No recipe found</Text>
+  }
+
   return (
-    <View className="py-6">
+    <View className="flex-1 py-6">
       <Text className="text-black dark:text-white text-3xl font-bold">
         Fetched Random Recipe
       </Text>
-      <Card className="m-4">
-        <CardHeader>
-          <CardTitle>Manual Setup Card</CardTitle>
-          <CardDescription>This card was added manually.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Text>This is the main content area of the card.</Text>
-        </CardContent>
-        <CardFooter>
-          <Text>Footer content goes here.</Text>
-        </CardFooter>
-      </Card>
+      <RecipeCard recipe={recipe} />
     </View>
   )
 }
---
+++ frontend/random-recipe-router-mobile/components/RecipeCard.tsx
@@ -0,0 +1,62 @@
+import { Text, View, ScrollView, Button } from 'react-native'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card'
+import { Recipe } from '../schemas/recipeSchema'
+import { Image } from 'react-native'
+
+interface RecipeCardProps {
+  recipe: Recipe
+}
+
+export default function RecipeCard({ recipe }: RecipeCardProps) {
+  return (
+    <Card className="m-4 flex-1">
+      <CardHeader>
+        <CardTitle>{recipe.title}</CardTitle>
+        <CardDescription>Description</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1">
+        <ScrollView>
+          {/* Image */}
+          <View className="relative mb-4 aspect-video w-full">
+            <Image
+              className="rounded-md"
+              source={{ uri: recipe.imageUrl }}
+              resizeMode="cover"
+              style={{ width: '100%', height: '100%' }}
+            />
+          </View>
+
+          {/* Ingredients */}
+          <View className="mt-4 rounded-lg border px-4 py-4">
+            <Text className="mb-2 text-lg font-semibold">Ingredients:</Text>
+            {recipe.ingredients.map((ingredient, i) => (
+              <Text key={i}>
+                {'• '}
+                {ingredient}
+              </Text>
+            ))}
+          </View>
+
+          {/* Instructions */}
+          <View className="mt-4 rounded-lg border px-4 py-4">
+            <Text className="mb-2 text-lg font-semibold">Instructions:</Text>
+            <Text>{recipe.instructions}</Text>
+          </View>
+        </ScrollView>
+      </CardContent>
+      <CardFooter>
+        <View className="flex-row justify-between w-full">
+          <Button color="black" title="Save Recipe" />
+          <Button color="gray" title="New Recipe" />
+        </View>
+      </CardFooter>
+    </Card>
+  )
+}
