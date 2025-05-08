f70fda071c498885eb2acc550c34e0073c98f923
cw222kq <lotten_w@hotmail.com>
2025-05-08 22:28:10 +0200

    Add AccountController with google login

---
+++ RandomRecipeGenerator.API/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Auth.AspNetCore3;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace RandomRecipeGenerator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet("login-google")]
+        public IActionResult LoginGoogle()
+        {
+            var properties = new AuthenticationProperties
+            {
+                RedirectUri = Url.Action(nameof(GoogleLoginCallback))
+            };
+            // Redirect to the Google login page
+            return Challenge(properties, GoogleOpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [HttpGet("google-login-callback")]
+        public IActionResult GoogleLoginCallback()
+        {
+            if (User.Identity == null || !User.Identity.IsAuthenticated)
+            {
+                return BadRequest("External login failed.");
+            }
+
+            var userId = User.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            var email = User.FindFirst(System.Security.Claims.ClaimTypes.Email)?.Value;
+            var firstName = User.FindFirst(System.Security.Claims.ClaimTypes.GivenName)?.Value;
+            var lastName = User.FindFirst(System.Security.Claims.ClaimTypes.Surname)?.Value;
+            var name = User.FindFirst("name")?.Value; 
+
+            return Ok(new { Message = "Login successful", UserId = userId, Email = email, Name = name, FirstName = firstName, LastName = lastName });
+        }
+    }
+}
