1967e83814b0b412596790a240b55c14194daf95
cw222kq <lotten_w@hotmail.com>
2025-05-11 22:00:49 +0200

    Modify the GoogleSigninButton.tsx and config nextjs to use https

--- frontend/random-recipe-web/src/components/GoogleSignInButton.tsx
+++ frontend/random-recipe-web/src/components/GoogleSignInButton.tsx
@@ -3,5 +3,11 @@
 import GoogleButton from 'react-google-button'
 
 export default function GoogleSignInButton() {
-  return <GoogleButton onClick={() => console.log('Google sign in')} />
+  return (
+    <GoogleButton
+      onClick={() =>
+        (window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/account/login-google`)
+      }
+    />
+  )
 }
