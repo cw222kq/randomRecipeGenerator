de39c8b39fa86aa94eb34b9b59c8a4d00c94c854
cw222kq <lotten_w@hotmail.com>
2025-05-13 16:59:56 +0200

    Redirect user to the hello page after login, created UserDTO

---
+++ RandomRecipeGenerator.API/Models/DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomRecipeGenerator.API.Models.DTO
+{
+    public class UserDTO
+    {
+        public required string GoogleUserId { get; set; }
+        public required string Email { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
