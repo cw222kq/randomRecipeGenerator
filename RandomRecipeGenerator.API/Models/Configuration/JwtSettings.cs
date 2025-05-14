aa2df08f5ed8cf0da574ae2d81a812ef31e0cbc5
cw222kq <lotten_w@hotmail.com>
2025-05-14 18:13:14 +0200

    Add JwtSettings

---
+++ RandomRecipeGenerator.API/Models/Configuration/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomRecipeGenerator.API.Models.Configuration
+{
+    public class JwtSettings
+    {
+        public string Key { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public int ExpiryInMinutes { get; set; } = 15; // Default value, 15 minutes
+    }
+}
