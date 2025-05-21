208e239f72a67aea079c8183f7e70c8763408e4f
cw222kq <lotten_w@hotmail.com>
2025-05-21 23:09:15 +0200

    Create baseService with generic get request

---
+++ frontend/random-recipe-web/src/services/baseService.ts
@@ -0,0 +1,29 @@
+interface GetOptions {
+  credentials?: RequestCredentials
+}
+
+const get = async <T>(
+  endpoint: string,
+  options: GetOptions = {},
+): Promise<T | null> => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}${endpoint}`,
+      options,
+    )
+
+    if (!response.ok) {
+      console.error(
+        `Failed to get data: ${response.status} ${response.statusText}`,
+      )
+      return null
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error('Error getting data', error)
+    return null
+  }
+}
+
+export default get
