f9fe7c75b61bc856971b3d5c9a7de7582329bc39
cw222kq <lotten_w@hotmail.com>
2025-05-22 22:53:35 +0200

    Update the userService to use the generic get and validation functions

--- frontend/random-recipe-web/src/services/userService.ts
+++ frontend/random-recipe-web/src/services/userService.ts
@@ -1,32 +1,14 @@
 import { userSchema, User } from '@/schemas/userSchema'
+import get from './baseService'
+import validateData from '@/lib/validation'
 
 const getLoggedInUser = async (): Promise<User | null> => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/account/user`,
-      { credentials: 'include' },
-    )
-
-    if (!response.ok) {
-      console.error(
-        `Failed to fetch logged in user: ${response.status} ${response.statusText}`,
-      )
-      return null
-    }
-
-    const user = await response.json()
-    const validatedUser = userSchema.safeParse(user)
-
-    if (!validatedUser.success) {
-      console.error('User validation failed:', validatedUser.error.format())
-      return null
-    }
-
-    return validatedUser.data
-  } catch (error) {
-    console.error('Error fetching logged in user:', error)
-    return null
-  }
+  const loggedInUser = await get<User>(
+    '/api/account/user',
+    { credentials: 'include' },
+    'logged in user',
+  )
+  return validateData(loggedInUser, userSchema, 'logged in user')
 }
 
 export default getLoggedInUser
