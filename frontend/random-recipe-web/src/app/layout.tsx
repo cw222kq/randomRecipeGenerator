2f0e4e4fa784855f325305358e32ab8479b7db5a
cw222kq <lotten_w@hotmail.com>
2025-05-26 21:58:09 +0200

    Add providers and make NavBar children in the layout children of the provider

--- frontend/random-recipe-web/src/app/layout.tsx
+++ frontend/random-recipe-web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import Navbar from '../components/general/Navbar'
+import { Providers } from './providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} mx-auto max-w-7xl px-4 antialiased sm:px-6 lg:px-8`}
       >
-        <Navbar />
-        {children}
+        <Providers>
+          <Navbar />
+          {children}
+        </Providers>
       </body>
     </html>
   )
