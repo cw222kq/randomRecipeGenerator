df0dad40627b83858a44316851b134f6df5d39cc
cw222kq <lotten_w@hotmail.com>
2025-05-26 22:06:19 +0200

    Add login and logout actions

--- frontend/random-recipe-web/src/store/features/auth/authSlice.ts
+++ frontend/random-recipe-web/src/store/features/auth/authSlice.ts
@@ -1,10 +1,21 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { initialAuthState } from '../../../schemas/authStateSchema'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { initialAuthState } from '@/schemas/authStateSchema'
+import { User } from '@/schemas/userSchema'
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: initialAuthState,
-  reducers: {},
+  reducers: {
+    login: (state, action: PayloadAction<User>) => {
+      state.user = action.payload
+      state.isAuthenticated = true
+    },
+    logout: (state) => {
+      state.user = null
+      state.isAuthenticated = false
+    },
+  },
 })
 
+export const { login, logout } = authSlice.actions
 export default authSlice.reducer
