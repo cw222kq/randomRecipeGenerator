362239381c4dfa1a740cbaef4a45e8e639d32e1c
cw222kq <lotten_w@hotmail.com>
2025-05-30 21:45:50 +0200

    Add conditionally render based on if the user is logged in or not

--- frontend/random-recipe-web/src/components/general/Navbar.tsx
+++ frontend/random-recipe-web/src/components/general/Navbar.tsx
@@ -1,11 +1,43 @@
+'use client'
+
 import Link from 'next/link'
 import GoogleSignInButton from '@/components/GoogleSignInButton'
 import SignOutButton from '@/components/SignOutButton'
 import RecipeGeneratorLogo from '@/components/icons/RecipeGeneratorLogo'
+import { useAppSelector } from '@/store/hooks'
+import { useEffect, useState } from 'react'
 
 export default function Navbar() {
+  const { user } = useAppSelector((state) => state.auth)
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+      /* TODO: Add small device menu when buttons are hidden */
+      <nav className="flex items-center justify-between border-b border-gray-200 py-5">
+        <div className="flex items-center gap-8">
+          <Link className="transition-all ease-in-out hover:scale-105" href="/">
+            <RecipeGeneratorLogo />
+          </Link>
+          <div className="hidden items-center gap-6 transition-colors sm:flex">
+            <Link
+              className="text-sm font-medium transition-all ease-in-out hover:scale-110 hover:text-blue-500"
+              href="/"
+            >
+              <p>Home</p>
+            </Link>
+          </div>
+        </div>
+        <div className="hidden ease-in-out hover:scale-105 sm:flex sm:items-center"></div>
+      </nav>
+    )
+  }
+
   return (
-    /* TODO: Add small device menu when buttons are hidden */
     <nav className="flex items-center justify-between border-b border-gray-200 py-5">
       <div className="flex items-center gap-8">
         <Link className="transition-all ease-in-out hover:scale-105" href="/">
@@ -19,20 +51,20 @@ export default function Navbar() {
           >
             <p>Home</p>
           </Link>
-          <Link
-            className="text-sm font-medium transition-all ease-in-out hover:scale-110 hover:text-blue-500"
-            href="/hello"
-          >
-            <p>Another Page</p>
-          </Link>
+          {user && (
+            <Link
+              className="text-sm font-medium transition-all ease-in-out hover:scale-110 hover:text-blue-500"
+              href="/hello"
+            >
+              <p>My Page</p>
+            </Link>
+          )}
         </div>
       </div>
 
       <div className="hidden ease-in-out hover:scale-105 sm:flex sm:items-center">
-        <GoogleSignInButton />
-      </div>
-      <div className="hidden ease-in-out hover:scale-105 sm:flex sm:items-center">
-        <SignOutButton />
+        {!user && <GoogleSignInButton />}
+        {user && <SignOutButton />}
       </div>
     </nav>
   )
