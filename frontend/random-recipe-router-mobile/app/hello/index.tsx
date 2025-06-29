03d16ddc4dc04a4e4f98950e5e2d3134df77d68a
cw222kq <lotten_w@hotmail.com>
2025-06-29 21:43:30 +0200

    feat: modify the hello page to use redux auth state

--- frontend/random-recipe-router-mobile/app/hello/index.tsx
+++ frontend/random-recipe-router-mobile/app/hello/index.tsx
@@ -1,23 +1,61 @@
 import { View, Text } from 'react-native'
-import { useEffect, useState } from 'react'
+import { useEffect, useCallback } from 'react'
 import { secureStorage } from '@/lib/secureStorage'
-import { User } from '@/schemas/userSchema'
+import { useAppSelector, useAppDispatch } from '@/store/hooks'
+import { setLoading, login } from '@/store/features/auth/authSlice'
 
 export default function Hello() {
-  const [user, setUser] = useState<User | null>(null)
+  const { user, isLoading, isAuthenticated } = useAppSelector(
+    (state) => state.auth,
+  )
+  const dispatch = useAppDispatch()
 
-  const fetchUser = async () => {
-    const user = await secureStorage.getUserData()
-    setUser(user)
-  }
+  const restoreUserSession = useCallback(async () => {
+    try {
+      dispatch(setLoading(true))
+      const storedUser = await secureStorage.getUserData()
+      const token = await secureStorage.getAppToken()
+      const isTokenExpired = await secureStorage.isTokenExpired()
+      if (!storedUser) {
+        console.error('No user data found')
+        return
+      }
+      if (storedUser && token && !isTokenExpired) {
+        dispatch(login(storedUser))
+      }
+    } catch (error) {
+      console.error('Error restoring user session:', error)
+    } finally {
+      dispatch(setLoading(false))
+    }
+  }, [dispatch])
 
   useEffect(() => {
-    fetchUser()
-  }, [])
+    restoreUserSession()
+  }, [restoreUserSession])
+
+  if (isLoading) {
+    return (
+      <View className="py-6">
+        <Text className="text-black dark:text-white text-lg">Loading...</Text>
+      </View>
+    )
+  }
+
+  if (!isAuthenticated || !user) {
+    return (
+      <View className="py-6">
+        <Text className="text-black dark:text-white text-lg">
+          Please sign in to continue
+        </Text>
+      </View>
+    )
+  }
+
   return (
     <View className="py-6">
       <Text className="text-black dark:text-white text-3xl font-bold">
-        Welcome {user?.firstName} {user?.lastName}
+        Welcome {user.firstName} {user.lastName}
       </Text>
     </View>
   )
