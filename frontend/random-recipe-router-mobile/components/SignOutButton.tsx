98722ac50c8c7d1f81c02d6ddd7e2001bc2c2586
cw222kq <lotten_w@hotmail.com>
2025-07-06 22:21:14 +0200

    feat: add the signOut functionality to the SignOutButton

--- frontend/random-recipe-router-mobile/components/SignOutButton.tsx
+++ frontend/random-recipe-router-mobile/components/SignOutButton.tsx
@@ -1,9 +1,19 @@
 import { Pressable, Text, View } from 'react-native'
+import { useGoogleAuth } from '@/hooks/useGoogleAuth'
 
 export default function SignOutButton() {
+  const { signOut } = useGoogleAuth()
+
+  const handleSignOut = async () => {
+    await signOut()
+  }
+
   return (
     <View>
-      <Pressable className="flex-row items-center pl-2 pr-4 py-2 bg-white border border-gray-300 rounded">
+      <Pressable
+        onPress={handleSignOut}
+        className="flex-row items-center pl-2 pr-4 py-2 bg-white border border-gray-300 rounded"
+      >
         <Text className="text-gray-700">Sign Out</Text>
       </Pressable>
     </View>
