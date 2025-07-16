d2e2502ad0d73fa680e1e967e46178798a8ddbf1
cw222kq <lotten_w@hotmail.com>
2025-07-16 20:57:14 +0200

    refactor: Clean up redudant restoration code

--- frontend/random-recipe-router-mobile/app/hello/index.tsx
+++ frontend/random-recipe-router-mobile/app/hello/index.tsx
@@ -1,38 +1,10 @@
 import { View, Text } from 'react-native'
-import { useEffect, useCallback } from 'react'
-import { secureStorage } from '@/lib/secureStorage'
-import { useAppSelector, useAppDispatch } from '@/store/hooks'
-import { setLoading, login } from '@/store/features/auth/authSlice'
+import { useAppSelector } from '@/store/hooks'
 
 export default function Hello() {
   const { user, isLoading, isAuthenticated } = useAppSelector(
     (state) => state.auth,
   )
-  const dispatch = useAppDispatch()
-
-  const restoreUserSession = useCallback(async () => {
-    try {
-      dispatch(setLoading(true))
-      const storedUser = await secureStorage.getUserData()
-      const token = await secureStorage.getAppToken()
-      const isTokenExpired = await secureStorage.isTokenExpired()
-      if (!storedUser) {
-        console.error('No user data found')
-        return
-      }
-      if (storedUser && token && !isTokenExpired) {
-        dispatch(login(storedUser))
-      }
-    } catch (error) {
-      console.error('Error restoring user session:', error)
-    } finally {
-      dispatch(setLoading(false))
-    }
-  }, [dispatch])
-
-  useEffect(() => {
-    restoreUserSession()
-  }, [restoreUserSession])
 
   if (isLoading) {
     return (
