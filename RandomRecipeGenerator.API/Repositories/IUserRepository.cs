aae15c79d94c45c3c7a5ec945317b57d0c966bc3
cw222kq <lotten_w@hotmail.com>
2025-07-22 22:30:21 +0200

    test: add more tests to UserRepositorytests, red phase

--- RandomRecipeGenerator.API/Repositories/IUserRepository.cs
+++ RandomRecipeGenerator.API/Repositories/IUserRepository.cs
@@ -7,8 +7,8 @@ namespace RandomRecipeGenerator.API.Repositories
         Task<User?> GetByGoogleUserIdAsync(string googleUserId);
         Task<User?> GetByEmailAsync(string email);
         Task<User?> GetByIdAsync(Guid id);
-        Task<User> CreateAsync(User user);
-        Task<User> UpdateAsync(User user);
+        Task<User?> CreateAsync(User user);
+        Task<User?> UpdateAsync(User user);
         Task<bool> DeleteAsync(Guid id);
     }
 }
