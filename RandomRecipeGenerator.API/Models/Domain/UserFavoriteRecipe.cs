382e982b16aac359d12cbc89086bb9afae011428
cw222kq <lotten_w@hotmail.com>
2025-08-04 22:54:02 +0200

    feat: add UserFavoriteRecipe join entity

---
+++ RandomRecipeGenerator.API/Models/Domain/UserFavoriteRecipe.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RandomRecipeGenerator.API.Models.Domain
+{
+    public class UserFavoriteRecipe
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public required Guid UserId { get; set; }
+
+        [Required]
+        public Guid RecipeId { get; set; }
+
+        // When the favorite was added
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation properties
+        public User User { get; set; } = null!;
+        public Recipe Recipe { get; set; } = null!;
+    }
+}
