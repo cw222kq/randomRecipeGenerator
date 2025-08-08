ca232f338fbd5e1fe95d2224608ffd1550417c7e
cw222kq <lotten_w@hotmail.com>
2025-08-08 21:10:02 +0200

    Test: add more tests for UserFavoriteRespository and make it build successfully

---
+++ RandomRecipeGenerator.API/Repositories/UserFavoriteRepository.cs
@@ -0,0 +1,31 @@
+﻿using RandomRecipeGenerator.API.Data;
+using RandomRecipeGenerator.API.Models.Domain;
+
+namespace RandomRecipeGenerator.API.Repositories
+{
+    public class UserFavoriteRepository(ApplicationDbContext context, ILogger<UserFavoriteRepository> logger) : IUserFavoriteRepository
+    {
+        private readonly ApplicationDbContext _context = context;
+        private readonly ILogger<UserFavoriteRepository> _logger = logger;
+
+        public Task<UserFavoriteRecipe?> AddFavoriteAsync(Guid UserId, Guid RecipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetUserFavoritesAsync(Guid UserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsFavoriteAsync(Guid UserId, Guid RecipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFavoriteAsync(Guid UserId, Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
