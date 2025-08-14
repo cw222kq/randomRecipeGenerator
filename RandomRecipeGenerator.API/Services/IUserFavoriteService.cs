f43609e82e36cdc490f94bc2c6b1c42814748b96
cw222kq <lotten_w@hotmail.com>
2025-08-14 21:13:59 +0200

    add tests for user favorite service and empty implementation, red phase

---
+++ RandomRecipeGenerator.API/Services/IUserFavoriteService.cs
@@ -0,0 +1,12 @@
+﻿using RandomRecipeGenerator.API.Models.Domain;
+
+namespace RandomRecipeGenerator.API.Services
+{
+    public interface IUserFavoriteService
+    {
+        Task<UserFavoriteRecipe?> AddFavoriteAsync(Guid userId, Guid recipeId);
+        Task<bool> RemoveFavoriteAsync(Guid userId, Guid recipeId);
+        Task<bool> IsFavoriteAsync(Guid userId, Guid recipeId);
+        Task<IEnumerable<Recipe>> GetUserFavoritesAsync(Guid userId);
+    }
+}
---
+++ RandomRecipeGenerator.API/Services/UserFavoriteService.cs
@@ -0,0 +1,31 @@
+﻿using RandomRecipeGenerator.API.Models.Domain;
+using RandomRecipeGenerator.API.Repositories;
+
+namespace RandomRecipeGenerator.API.Services
+{
+    public class UserFavoriteService(IUserFavoriteRepository repository, ILogger<UserFavoriteService> logger) : IUserFavoriteService
+    {
+        private readonly IUserFavoriteRepository _repository = repository;
+        private readonly ILogger<UserFavoriteService> _logger = logger;
+
+        public Task<UserFavoriteRecipe?> AddFavoriteAsync(Guid userId, Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Recipe>> GetUserFavoritesAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsFavoriteAsync(Guid userId, Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFavoriteAsync(Guid userId, Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
