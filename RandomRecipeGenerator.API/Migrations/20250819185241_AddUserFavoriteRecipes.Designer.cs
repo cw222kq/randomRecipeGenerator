0d38324960e31eda323feedbbde40ef1c8135be0
cw222kq <lotten_w@hotmail.com>
2025-08-19 21:14:13 +0200

    feat: add db migration for UserFavoriteRecipes table

--- RandomRecipeGenerator.API/Migrations/20250819185241_AddUserFavoriteRecipes.Designer.cs
+++ RandomRecipeGenerator.API/Migrations/20250819185241_AddUserFavoriteRecipes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RandomRecipeGenerator.API.Data;
@@ -12,9 +13,11 @@ using RandomRecipeGenerator.API.Data;
 namespace RandomRecipeGenerator.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250819185241_AddUserFavoriteRecipes")]
+    partial class AddUserFavoriteRecipes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
