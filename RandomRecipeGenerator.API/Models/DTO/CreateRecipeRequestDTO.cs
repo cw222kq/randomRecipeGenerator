e646df1ab9d9e7568d5826ffe734dd1182293b18
cw222kq <lotten_w@hotmail.com>
2025-08-23 20:33:41 +0200

    feat: add CreateRecipeRequestDTO

---
+++ RandomRecipeGenerator.API/Models/DTO/CreateRecipeRequestDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RandomRecipeGenerator.API.Models.DTO
+{
+    public class CreateRecipeRequestDTO
+    {
+        [Required]
+        [StringLength(255, MinimumLength =1)]
+        public required string Title { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public required List<string> Ingredients { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public required string Instructions { get; set; }
+
+        [StringLength(1000)]
+        public string? ImageUrl { get; set; }
+    }
+}
