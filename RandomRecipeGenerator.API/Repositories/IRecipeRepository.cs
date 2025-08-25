1ba55ca29f14d75f30e9189a67a13787d619c332
cw222kq <lotten_w@hotmail.com>
2025-08-25 20:09:54 +0200

    feat: add IRecipeRepository interface

---
+++ RandomRecipeGenerator.API/Repositories/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using RandomRecipeGenerator.API.Models.Domain;
+
+namespace RandomRecipeGenerator.API.Repositories
+{
+    public interface IRecipeRepository
+    {
+        Task<Recipe?> CreateRecipeAsync(Recipe recipe);
+        Task<Recipe?> GetRecipeByIdAsync(Guid id);
+        Task<IEnumerable<Recipe>> GetUserRecipesAsync(Guid userId);
+        Task<Recipe?> UpdateRecipeAsync(Recipe recipe);
+        Task<bool> DeleteRecipeAsync(Guid id);
+        Task<bool> IsRecipeOwnerAsync(Guid recipeId, Guid userId);
+    }
+}
