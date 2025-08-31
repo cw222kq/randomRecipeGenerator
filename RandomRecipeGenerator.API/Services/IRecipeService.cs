c503ec3a6f1e327e432b7348476f1061355c74e9
cw222kq <lotten_w@hotmail.com>
2025-08-31 22:10:12 +0200

    test: add tests for RecipeService, and add IRecipeService and RecipeService with minimal implemenation to make the code compile, red phase

---
+++ RandomRecipeGenerator.API/Services/IRecipeService.cs
@@ -0,0 +1,14 @@
+﻿using RandomRecipeGenerator.API.Models.Domain;
+
+namespace RandomRecipeGenerator.API.Services
+{
+    public interface IRecipeService
+    {
+        Task<Recipe?> CreateUserRecipeAsync(Guid UserId, string title, List<string> ingredients, string instructions, string? imageUrl = null);
+        Task<Recipe?> GetRecipeByIdAsync(Guid Id);
+        Task<IEnumerable<Recipe>> GetUserRecipesAsync(Guid userId);
+        Task<Recipe?> UpdateUserRecipeAsync(Guid recipeId, Guid userId, string title, List<string> ingredients, string instructions, string? imageUrl = null);
+        Task<bool> DeleteUserRecipeAsync(Guid recipeId, Guid userId);
+        Task<bool> IsRecipeOwnerAsync(Guid recipeId, Guid userId);
+    }
+}
