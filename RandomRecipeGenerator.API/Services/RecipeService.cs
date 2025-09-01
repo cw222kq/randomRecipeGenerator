0a03f2936bd40be2ecd8fef63e5bf10787079110
cw222kq <lotten_w@hotmail.com>
2025-09-01 21:09:00 +0200

    feat: implement minimal functionality to pass the tests for CreateUserRecipeAsync, green phase

--- RandomRecipeGenerator.API/Services/RecipeService.cs
+++ RandomRecipeGenerator.API/Services/RecipeService.cs
@@ -8,9 +8,24 @@ namespace RandomRecipeGenerator.API.Services
         private readonly IRecipeRepository _repository = repository;
         private readonly ILogger<RecipeService> _logger = logger;
 
-        public Task<Recipe?> CreateUserRecipeAsync(Guid UserId, string title, List<string> ingredients, string instructions, string? imageUrl = null)
+        public async Task<Recipe?> CreateUserRecipeAsync(Guid UserId, string title, List<string> ingredients, string instructions, string? imageUrl = null)
         {
-            throw new NotImplementedException();
+            if (UserId == Guid.Empty || string.IsNullOrWhiteSpace(title) || ingredients == null || ingredients.Count == 0 || string.IsNullOrWhiteSpace(instructions))
+            {
+                return null;
+            }
+
+            var recipe = new Recipe
+            {
+                Title = title,
+                SpoonacularId = 0,
+                Ingredients = ingredients,
+                Instructions = instructions,
+                ImageUrl = imageUrl,
+                UserId = UserId,
+            };
+
+            return await _repository.CreateRecipeAsync(recipe);
         }
 
         public Task<bool> DeleteUserRecipeAsync(Guid recipeId, Guid userId)
