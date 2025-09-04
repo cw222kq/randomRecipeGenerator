2767a82cb11eb38400cf3bb841acc4978b55dcd6
cw222kq <lotten_w@hotmail.com>
2025-09-04 20:31:54 +0200

    feat: add AutoMapper profiles for RecipeRequestDTO

--- RandomRecipeGenerator.API/Mappings/AutoMapperProfiles.cs
+++ RandomRecipeGenerator.API/Mappings/AutoMapperProfiles.cs
@@ -10,6 +10,16 @@ namespace RandomRecipeGenerator.API.Mappings
         {
             CreateMap<Recipe, RecipeDTO>().ReverseMap();
             CreateMap<User, UserDTO>().ReverseMap();
+
+            // Recipe request mapping - for incoming requests (from DTO to Domain)
+            CreateMap<RecipeRequestDTO, Recipe>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.SpoonacularId, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore())
+                .ForMember(dest => dest.UserFavoriteRecipes, opt => opt.Ignore());
         }
     }
 }
