b526b3611726819d2c81b19248cd06b84a3dc573
cw222kq <lotten_w@hotmail.com>
2025-10-01 22:49:24 +0200

    feat: add more sections to the form

--- frontend/random-recipe-web/src/components/RecipeForm.tsx
+++ frontend/random-recipe-web/src/components/RecipeForm.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
 
 export default function RecipeForm() {
   return (
@@ -22,6 +23,45 @@ export default function RecipeForm() {
               required
             />
           </div>
+
+          {/* Ingredients */}
+          <div>
+            <Label htmlFor="ingredients">Ingredients *</Label>
+            <div className="flex flex-wrap gap-2 p-2">
+              <span className="text-sm">Ingredient 1</span>
+            </div>
+
+            {/* Input for adding new ingredient */}
+            <div className="flex gap-2">
+              <Input
+                id="ingredients"
+                type="text"
+                placeholder="Enter ingredients"
+                className="flex-1"
+              />
+              <Button type="button" variant="secondary">
+                Add
+              </Button>
+            </div>
+          </div>
+
+          {/* Instructions */}
+          <div className="space-y-2">
+            <Label htmlFor="instructions">Instructions *</Label>
+            <Textarea
+              id="instructions"
+              placeholder="Enter instructions"
+              className="resize-none"
+              required
+            />
+          </div>
+
+          {/* Image URL*/}
+          <div className="space-y-2">
+            <Label htmlFor="imageUrl">Image URL (optional)</Label>
+            <Input id="imageUrl" type="url" placeholder="Enter image URL" />
+          </div>
+
           <Button type="submit" className="w-full">
             Save Recipe
           </Button>
