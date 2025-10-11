e429953499c8bdcf47414bfe3ca17347a262f1ed
cw222kq <lotten_w@hotmail.com>
2025-10-11 21:15:39 +0200

    fix: update Recipe domain model to allow nullable SpoonacularId

--- RandomRecipeGenerator.API/Migrations/20251011184709_MakeSpoonacularIdNullable.Designer.cs
+++ RandomRecipeGenerator.API/Migrations/20251011184709_MakeSpoonacularIdNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RandomRecipeGenerator.API.Data;
@@ -12,9 +13,11 @@ using RandomRecipeGenerator.API.Data;
 namespace RandomRecipeGenerator.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251011184709_MakeSpoonacularIdNullable")]
+    partial class MakeSpoonacularIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -45,6 +48,7 @@ namespace RandomRecipeGenerator.API.Migrations
                         .HasColumnType("text");
 
                     b.Property<int?>("SpoonacularId")
+                        .IsRequired()
                         .HasColumnType("integer");
 
                     b.Property<string>("Title")
