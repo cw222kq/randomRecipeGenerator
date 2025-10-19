9aa356f3968e15038d8ed80e1d5e0ee85e00aa6e
cw222kq <lotten_w@hotmail.com>
2025-10-19 21:10:52 +0200

    feat: add RecipeDetailModal component

---
+++ frontend/random-recipe-web/src/components/RecipeDetailModal.tsx
@@ -0,0 +1,41 @@
+import { Recipe } from '@/schemas/recipeSchema'
+import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
+interface RecipeDetailModalProps {
+  recipe: Recipe | null
+  isOpen: boolean
+  onClose: () => void
+}
+
+export default function RecipeDetailModal({
+  recipe,
+  isOpen,
+  onClose,
+}: RecipeDetailModalProps) {
+  if (!isOpen || !recipe) {
+    return null
+  }
+  return (
+    <div>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>{recipe.title}</CardTitle>
+            <Button
+              variant="ghost"
+              size="sm"
+              onClick={onClose}
+              className="h-8 w-8 p-0"
+            >
+              X
+            </Button>
+          </CardHeader>
+          <CardContent>
+            <p>The recipe informmation</p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
+}
