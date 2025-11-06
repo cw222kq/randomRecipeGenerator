50808bb1030f7a918775f542073659391907b7ab
cw222kq <lotten_w@hotmail.com>
2025-11-06 21:27:05 +0100

    fix: update the Image tag to include sizes allign with nextjs documentation

--- frontend/random-recipe-web/src/components/RecipeDetailModal.tsx
+++ frontend/random-recipe-web/src/components/RecipeDetailModal.tsx
@@ -221,6 +221,7 @@ export default function RecipeDetailModal({
                     }
                     alt={isEditing ? editData.title : recipe.title}
                     fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     className="object-cover"
                   />
                 </div>
