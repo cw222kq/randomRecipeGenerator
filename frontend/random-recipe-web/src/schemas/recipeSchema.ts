6c2d0e86c87433a45ba073fbe8f22f7e9d598c69
cw222kq <lotten_w@hotmail.com>
2025-11-15 19:48:38 +0100

    feat: update recipe schema to allowed null for spoonacularId

--- frontend/random-recipe-web/src/schemas/recipeSchema.ts
+++ frontend/random-recipe-web/src/schemas/recipeSchema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 // Define and export the Zod schema
 export const RecipeSchema = z.object({
   id: z.string(),
-  spoonacularId: z.number().optional(),
+  spoonacularId: z.number().nullable().optional(),
   title: z.string(),
   ingredients: z.array(z.string()),
   instructions: z.string(),
