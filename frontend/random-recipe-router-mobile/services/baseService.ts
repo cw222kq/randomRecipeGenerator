2c7537106f853ff465152ebbc4ab27f15f9689aa
cw222kq <lotten_w@hotmail.com>
2025-12-27 14:49:20 +0100

    feat: add a generic post request function

--- frontend/random-recipe-router-mobile/services/baseService.ts
+++ frontend/random-recipe-router-mobile/services/baseService.ts
@@ -27,4 +27,37 @@ const getRequest = async <T>(
   }
 }
 
-export { getRequest }
+const postRequest = async <T>(
+  endpoint: string,
+  data: unknown,
+  options: RequestOptions = {},
+  context: string = 'data',
+): Promise<T | null> => {
+  try {
+    const response = await fetch(
+      `${process.env.EXPO_PUBLIC_API_BASE_URL}${endpoint}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+        ...options,
+      },
+    )
+
+    if (!response.ok) {
+      console.error(
+        `Failed to post ${context}: ${response.status} ${response.statusText}`,
+      )
+      return null
+    }
+
+    return await response.json()
+  } catch (error) {
+    console.error(`Error posting ${context}`, error)
+    return null
+  }
+}
+
+export { getRequest, postRequest }
