1bf920cee904adb6dfff21de272d05ee92a67b3e
cw222kq <lotten_w@hotmail.com>
2026-02-13 20:40:34 +0100

    style: update style and add dark mode support to SignOutButton component

--- frontend/random-recipe-router-mobile/components/SignOutButton.tsx
+++ frontend/random-recipe-router-mobile/components/SignOutButton.tsx
@@ -21,13 +21,17 @@ export default function SignOutButton() {
       <Pressable
         onPress={handleSignOut}
         disabled={isLoading}
-        className={`... ${showConfirmation ? 'bg-red-500' : 'bg-white'} flex-row items-center pl-2 pr-4 py-2 bg-white border border-gray-300 rounded`}
+        className={`flex-row items-center pl-2 pr-4 py-2 border rounded-full ${showConfirmation ? 'bg-red-500 border-red-500' : 'bg-white border-gray-300 dark:bg-gray-800 dark:border-gray-600'}`}
       >
         <View className="mr-3">
           {isLoading && <ActivityIndicator size="small" color="#dc2626" />}
         </View>
 
-        <Text className={'text-gray-700'}>
+        <Text
+          className={
+            showConfirmation ? 'text-white' : 'text-gray-700 dark:text-gray-200'
+          }
+        >
           {isLoading
             ? 'Signing out...'
             : showConfirmation
