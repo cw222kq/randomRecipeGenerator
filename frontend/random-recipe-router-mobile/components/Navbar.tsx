3d1af91b25e62d427a0c9432ad5e4092bd333bce
cw222kq <lotten_w@hotmail.com>
2026-02-15 17:06:26 +0100

    style: add dark mode to Navbar border

--- frontend/random-recipe-router-mobile/components/Navbar.tsx
+++ frontend/random-recipe-router-mobile/components/Navbar.tsx
@@ -1,5 +1,4 @@
-import { View, Text } from 'react-native'
-import { Link } from 'expo-router'
+import { View } from 'react-native'
 import RecipeGeneratorLogo from '@/components/icons/RecipeGeneratorLogo'
 import GoogleSignInButton from '@/components/GoogleSignInButton'
 import SignOutButton from '@/components/SignOutButton'
@@ -9,7 +8,7 @@ export const Navbar = () => {
   const { user, isLoading } = useAppSelector((state) => state.auth)
 
   return (
-    <View className="flex-row items-center justify-between border-b border-gray-100 py-5">
+    <View className="flex-row items-center justify-between border-b border-gray-100 py-5 dark:border-gray-700">
       <RecipeGeneratorLogo />
 
       <View className="flex-row items-center gap-6">
